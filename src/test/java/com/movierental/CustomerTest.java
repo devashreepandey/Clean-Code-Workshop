f46108814df784d7ea1817ef0a19868387d7ed4f
Abhinav Manchanda <abhinav.manchanda@gmail.com>
2018-07-27 12:41:38 +0530

    Added a characterisation test for the customer's statement.

--- src/test/java/com/movierental/CustomerTest.java
+++ src/test/java/com/movierental/CustomerTest.java
@@ -2,8 +2,22 @@ package com.movierental;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class CustomerTest {
     @Test
-    public void test(){}
+    public void verifyThatTheGeneratedStatementIsCorrect(){
+        Customer customer = new Customer("Barack");
+        customer.addRental(new Rental(new Movie("Godfather", Movie.REGULAR), 3));
+        customer.addRental(new Rental(new Movie("Toy Story", Movie.CHILDRENS), 2));
+        customer.addRental(new Rental(new Movie("Avengers", Movie.NEW_RELEASE), 4));
+
+        assertEquals("Rental Record for Barack\n" +
+                "\tGodfather\t3.5\n" +
+                "\tToy Story\t1.5\n" +
+                "\tAvengers\t12.0\n" +
+                "Amount owed is 17.0\n" +
+                "You earned 4 frequent renter points", customer.statement());
+    }
 
 }
