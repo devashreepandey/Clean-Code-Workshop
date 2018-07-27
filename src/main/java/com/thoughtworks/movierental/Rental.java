b5b87c5c09755e390f91a27211c9bfb63d18f807
Abhinav Manchanda <abhinav.manchanda@gmail.com>
2018-07-27 13:21:33 +0530

    Inlined getters since they are no longer needed.

--- src/main/java/com/thoughtworks/movierental/Rental.java
+++ src/main/java/com/thoughtworks/movierental/Rental.java
@@ -20,19 +20,19 @@ public class Rental {
 
     double amount() {
       double thisAmount = 0;
-      switch (getMovie().getPriceCode()) {
+      switch (movie.getPriceCode()) {
         case Movie.REGULAR:
           thisAmount += 2;
-          if (getDaysRented() > 2)
-            thisAmount += (getDaysRented() - 2) * 1.5;
+          if (daysRented > 2)
+            thisAmount += (daysRented - 2) * 1.5;
           break;
         case Movie.NEW_RELEASE:
-          thisAmount += getDaysRented() * 3;
+          thisAmount += daysRented * 3;
           break;
         case Movie.CHILDRENS:
           thisAmount += 1.5;
-          if (getDaysRented() > 3)
-            thisAmount += (getDaysRented() - 3) * 1.5;
+          if (daysRented > 3)
+            thisAmount += (daysRented - 3) * 1.5;
           break;
       }
       return thisAmount;
