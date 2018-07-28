4fc20cc5b2eb179b056e81df212356a0cffcfedb
Abhinav Manchanda <abhinav.manchanda@gmail.com>
2018-07-28 20:30:50 +0530

    Used frequent renter points from price code.

--- src/main/java/com/thoughtworks/movierental/Movie.java
+++ src/main/java/com/thoughtworks/movierental/Movie.java
@@ -26,23 +26,27 @@ public class Movie {
   }
 
   double amount(int daysRented) {
-    PriceCode priceCode = null;
-      switch (getPriceCode()) {
-        case REGULAR:
-          priceCode = PriceCode.REGULAR();
-          break;
-        case NEW_RELEASE:
-          priceCode = PriceCode.NEW_RELEASE();
-          break;
-        case CHILDRENS:
-          priceCode = PriceCode.CHILDRENS();
-          break;
-      }
-      return priceCode.amount(daysRented);
+    return createPriceCode().amount(daysRented);
     }
 
+  private PriceCode createPriceCode() {
+    PriceCode priceCode = null;
+    switch (getPriceCode()) {
+      case REGULAR:
+        priceCode = PriceCode.REGULAR();
+        break;
+      case NEW_RELEASE:
+        priceCode = PriceCode.NEW_RELEASE();
+        break;
+      case CHILDRENS:
+        priceCode = PriceCode.CHILDRENS();
+        break;
+    }
+    return priceCode;
+  }
+
   int frequentRentalPoints(int daysRented) {
-    return getPriceCode() == NEW_RELEASE && daysRented > 1 ? 2 : 1;
+    return createPriceCode().frequentRentalPoints(daysRented);
   }
 }
 
