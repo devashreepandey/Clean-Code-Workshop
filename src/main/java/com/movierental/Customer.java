8548628dd0c9e1848f57bbce3cea5672687a4020
Abhinav Manchanda <abhinav.manchanda@gmail.com>
2018-10-18 20:38:43 +0530

    Extracted Frequent Renter Points, moved to Rental.

--- src/main/java/com/movierental/Customer.java
+++ src/main/java/com/movierental/Customer.java
@@ -21,16 +21,12 @@ public class Customer {
 
   public String statement() {
     double totalAmount = 0;
-    int frequentRenterPoints = 0;
+    int totalFrequentRenterPoints = 0;
     String result = "Rental Record for " + getName() + "\n";
     for (Rental rental : rentals) {
       double amount = rental.amount();
-      // add frequent renter points
-      frequentRenterPoints++;
-      // add bonus for a two day new release rental
-      if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-          &&
-          rental.getDaysRented() > 1) frequentRenterPoints++;
+
+      totalFrequentRenterPoints += rental.frequentRenterPoints();
 
       //show figures for this rental
       result += "\t" + rental.getMovie().getTitle() + "\t" +
@@ -40,7 +36,7 @@ public class Customer {
 
     //add footer lines result
     result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-    result += "You earned " + String.valueOf(frequentRenterPoints)
+    result += "You earned " + String.valueOf(totalFrequentRenterPoints)
         + " frequent renter points";
     return result;
   }
