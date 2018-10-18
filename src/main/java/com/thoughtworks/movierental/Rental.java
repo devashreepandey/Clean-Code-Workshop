356bb8e0beab4c0ecb9fcab3182b614288983415
Abhinav Manchanda <abhinav.manchanda@gmail.com>
2018-10-18 20:19:46 +0530

    Moved amount method to rental

--- src/main/java/com/thoughtworks/movierental/Rental.java
+++ src/main/java/com/thoughtworks/movierental/Rental.java
@@ -17,4 +17,24 @@ public class Rental {
   public Movie getMovie() {
     return movie;
   }
+
+    double amount() {
+      double amount = 0;
+      switch (movie.getPriceCode()) {
+        case Movie.REGULAR:
+          amount += 2;
+          if (daysRented > 2)
+            amount += (daysRented - 2) * 1.5;
+          break;
+        case Movie.NEW_RELEASE:
+          amount += daysRented * 3;
+          break;
+        case Movie.CHILDRENS:
+          amount += 1.5;
+          if (daysRented > 3)
+            amount += (daysRented - 3) * 1.5;
+          break;
+      }
+      return amount;
+    }
 }
