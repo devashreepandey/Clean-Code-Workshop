1d8f135fc489a9956b50e9722c722a9cefdd484d
Abhinav Manchanda <abhinav.manchanda@gmail.com>
2018-10-19 11:43:16 +0530

    Extracted total amount into a separate method.

--- src/main/java/com/thoughtworks/movierental/Customer.java
+++ src/main/java/com/thoughtworks/movierental/Customer.java
@@ -20,25 +20,30 @@ public class Customer {
   }
 
   public String statement() {
-    double totalAmount = 0;
     int totalFrequentRenterPoints = 0;
     String result = "Rental Record for " + getName() + "\n";
-    for (Rental rental : rentals) {
-      double amount = rental.amount();
 
+    for (Rental rental : rentals) {
       totalFrequentRenterPoints += rental.frequentRenterPoints();
 
       //show figures for this rental
       result += "\t" + rental.getMovie().getTitle() + "\t" +
-          String.valueOf(amount) + "\n";
-      totalAmount += amount;
+          String.valueOf(rental.amount()) + "\n";
     }
 
     //add footer lines result
-    result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+    result += "Amount owed is " + String.valueOf(totalAmount()) + "\n";
     result += "You earned " + String.valueOf(totalFrequentRenterPoints)
         + " frequent renter points";
     return result;
+  }
+
+  private double totalAmount() {
+    double totalAmount = 0;
+    for (Rental rental : rentals) {
+      totalAmount += rental.amount();
+    }
+    return totalAmount;
   }
 
 }
